e2dcd7a38f3f3005982a4bb5aafe187c75f5619b
hschow <47091224+hschow@users.noreply.github.com>
2019-11-19 15:33:24 -0800

    Add files via upload

--- Practice11Test.java
+++ Practice11Test.java
@@ -8,11 +8,20 @@ public class Practice11Test {
         int[][] t2 = new int[][]{{1,3},{2,1},{3,1},{2,3}};
         int[][] t3 = new int[][]{{1,2},{2,1},{1,3},{2,3},{4,1}};
         int[][] t4 = new int[][]{};
-        System.out.println(judy.findJudge(3,t1)); //If there is an judge then the numbetr that corresponds to the judge will be printed otherwise, -1 will be printed
-        System.out.println(judy.findJudge(3,t2));
-        System.out.println(judy.findJudge(4,t3));
-        System.out.println(judy.findJudge(0,t4));
-
+        int[][] t5 = new int[][]{{1,2}};
+        int[][] t6 = new int[][]{{1,3},{2,3}};
+        int[][] t7 = new int[][]{{1,3},{2,3},{3,1}};
+        int[][] t8 = new int[][]{{1,2},{2,3}};
+        int[][] t9 = new int[][]{{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}};
+        System.out.println(judy.findJudge(3, t1)); //If there is an judge then the numbetr that corresponds to the judge will be printed otherwise, -1 will be printed
+        System.out.println(judy.findJudge(3, t2));
+        System.out.println(judy.findJudge(4, t3));
+        System.out.println(judy.findJudge(0, t4));
+        System.out.println(judy.findJudge(2,t5));
+        System.out.println(judy.findJudge(3,t6));
+        System.out.println(judy.findJudge(3,t7));
+        System.out.println(judy.findJudge(3,t8));
+        System.out.println(judy.findJudge(4,t9));
     }
 
 }
